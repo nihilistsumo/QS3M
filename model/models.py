4f5b7a62721f104b8a2c3ced58f6b754f6f51062
Sumanta  Kashyapi <sk1105@wildcats.unh.edu>
2020-09-13 01:57:44 -0400

    test report include

--- model/models.py
+++ model/models.py
@@ -155,8 +155,13 @@ def run_model(qry_attn_file_train, qry_attn_file_test, train_pids_file, test_pid
                 ypred_val = m(X_val)
                 val_loss = mseloss(ypred_val, y_val)
                 val_auc = roc_auc_score(y_val.detach().cpu().numpy(), ypred_val.detach().cpu().numpy())
-                print('\rTrain loss: %.5f, Train auc: %.5f, Val loss: %.5f, Val auc: %.5f' %
-                      (loss.item(), auc, val_loss.item(), val_auc), end='')
+
+                ypred_test = m(X_test)
+                test_loss = mseloss(ypred_test, y_test)
+                test_auc = roc_auc_score(y_test.detach().cpu().numpy(), ypred_test.detach().cpu().numpy())
+
+                print('\rTrain loss: %.5f, Train auc: %.5f, Val loss: %.5f, Val auc: %.5f, Test loss: %.5f, Test auc: %.5f' %
+                      (loss.item(), auc, val_loss.item(), val_auc, test_loss.item(), test_auc), end='')
     m.eval()
     ypred_test = m(X_test)
     test_loss = mseloss(ypred_test, y_test)

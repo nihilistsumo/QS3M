162b9581c8b1d06c152cede04fb92b33688e282c
Sumanta  Kashyapi <sk1105@wildcats.unh.edu>
2020-09-15 16:48:23 -0400

    arch change

--- model/models.py
+++ model/models.py
@@ -135,8 +135,10 @@ def run_model(qry_attn_file_train, qry_attn_file_test, train_pids_file, test_pid
     '''
     X_train = X_train.cuda()
     y_train = y_train.cuda()
+    '''
     X_val = X_val.cuda()
     y_val = y_val.cuda()
+    '''
     X_test = X_test.cuda()
     y_test = y_test.cuda()
     '''
@@ -144,6 +146,8 @@ def run_model(qry_attn_file_train, qry_attn_file_test, train_pids_file, test_pid
     m = CATSSimilarityModel(768).cuda()
     opt = optim.Adam(m.parameters(), lr=lrate)
     mseloss = nn.MSELoss()
+    test_loss = 1.0
+    test_auc = 0.0
     for i in range(epochs):
         print('\nEpoch '+str(i+1))
         for b in range(math.ceil(train_samples//batch)):
@@ -157,17 +161,17 @@ def run_model(qry_attn_file_train, qry_attn_file_test, train_pids_file, test_pid
             opt.step()
             if b % 100 == 0:
                 m.eval()
-                m.cpu()
                 ypred_val = m(X_val)
                 val_loss = mseloss(ypred_val, y_val)
-                val_auc = roc_auc_score(y_val.detach().numpy(), ypred_val.detach().numpy())
-
+                val_auc = roc_auc_score(y_val.detach().cpu().numpy(), ypred_val.detach().cpu().numpy())
+                print(
+                    '\rTrain loss: %.5f, Train auc: %.5f, Val loss: %.5f, Val auc: %.5f, Test loss: %.5f, Test auc: %.5f' %
+                    (loss.item(), auc, val_loss.item(), val_auc, test_loss.item(), test_auc), end='')
+            if b % 1000 == 0:
+                m.cpu()
                 ypred_test = m(X_test)
                 test_loss = mseloss(ypred_test, y_test)
                 test_auc = roc_auc_score(y_test.detach().numpy(), ypred_test.detach().numpy())
-
-                print('\rTrain loss: %.5f, Train auc: %.5f, Val loss: %.5f, Val auc: %.5f, Test loss: %.5f, Test auc: %.5f' %
-                      (loss.item(), auc, val_loss.item(), val_auc, test_loss.item(), test_auc), end='')
                 m.cuda()
     m.eval()
     m.cpu()

14b0d8c3a4a157f4560f2de6bfb29c1f3b5b283e
Sumanta  Kashyapi <sk1105@wildcats.unh.edu>
2020-09-24 14:36:17 -0400

    sentwise model run

--- model/sent_models.py
+++ model/sent_models.py
@@ -159,7 +159,7 @@ def main():
     parser.add_argument('-tq', '--test_qids', default="by1test-context-qids.npy")
     parser.add_argument('-trqv', '--train_qvecs', default="half-y1train-context-qvecs.npy")
     parser.add_argument('-tqv', '--test_qvecs', default="by1test-context-qvecs.npy")
-    parser.add_argument('-lr', '--lrate', type=float, default=0.0001)
+    parser.add_argument('-lr', '--lrate', type=float, default=0.001)
     parser.add_argument('-bt', '--batch', type=int, default=32)
     parser.add_argument('-ep', '--epochs', type=int, default=5)
     parser.add_argument('--cache', action='store_true')

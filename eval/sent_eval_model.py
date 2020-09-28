98cd106ef7919027b915f03293912796b5f34c76
Sumanta  Kashyapi <sk1105@wildcats.unh.edu>
2020-09-27 21:17:07 -0400

    sent eval

--- eval/sent_eval_model.py
+++ eval/sent_eval_model.py
@@ -143,8 +143,8 @@ def main():
     parser.add_argument('-hq', '--hier_qrels', default="/home/sk1105/sumanta/trec_dataset/benchmarkY1/benchmarkY1-train-nodup/train.pages.cbor-toplevel.qrels")
     parser.add_argument('-pp', '--parapairs',
                         default="/home/sk1105/sumanta/Mule-data/input_data_v2/pairs/train-cleaned-parapairs/by1-train-cleaned.parapairs.json")
-    parser.add_argument('-tp', '--test_pids', default="by1train-all-pids.npy")
-    parser.add_argument('-tv', '--test_pvecs', default="by1train-all-paravecs.npy")
+    parser.add_argument('-tp', '--test_pids', default="by1train-all-pids-sentwise.npy")
+    parser.add_argument('-tv', '--test_pvecs', default="by1train-all-paravecs-sentwise.npy")
     parser.add_argument('-tq', '--test_qids', default="by1train-context-qids.npy")
     parser.add_argument('-tqv', '--test_qvecs', default="by1train-context-qvecs.npy")
     parser.add_argument('-cp', '--cats_path', default="/home/sk1105/sumanta/CATS/saved_models/cats_leadpara_b32_l0.00001_i3.model")

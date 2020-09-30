f14367edc085a99cf7ef483a2c8d5b8b4afbeb29
Sumanta  Kashyapi <sk1105@wildcats.unh.edu>
2020-09-30 17:33:20 -0400

    add ttest

--- eval/sent_eval_model.py
+++ eval/sent_eval_model.py
@@ -150,11 +150,11 @@ def main():
     parser.add_argument('-tv', '--test_pvecs', default="by1test-all-paravecs-sentwise.npy")
     parser.add_argument('-tq', '--test_qids', default="by1test-context-title-qids.npy")
     parser.add_argument('-tqv', '--test_qvecs', default="by1test-context-title-qvecs.npy")
-    parser.add_argument('-cp', '--cats_path', default="/home/sk1105/sumanta/CATS/saved_models/cats_title_b32_l0.00001_i3.model")
+    parser.add_argument('-cp', '--cats_path', default="/home/sk1105/sumanta/CATS/saved_models/cats_leadpara_b32_l0.00001_i3.model")
     parser.add_argument('-seq', '--max_seq', type=int, default=10)
     parser.add_argument('-pn', '--param_n', type=int, default=32)
     parser.add_argument('-mt', '--model_type', default="fcats")
-    parser.add_argument('-mp', '--model_path', default="/home/sk1105/sumanta/CATS/saved_models/sentcats_maxlen_10_title_b32_l0.0001_i6.model")
+    parser.add_argument('-mp', '--model_path', default="/home/sk1105/sumanta/CATS/saved_models/sentcats_maxlen_10_leadpara_b32_l0.0001_i6.model")
 
     '''
     parser.add_argument('-dd', '--data_dir', default="/home/sk1105/sumanta/CATS_data/")

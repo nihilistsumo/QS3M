4996505bad83af7b7170da7ae1456fb55a192973
Sumanta  Kashyapi <sk1105@wildcats.unh.edu>
2020-10-31 00:28:22 -0400

    saving cavs models

--- model/models.py
+++ model/models.py
@@ -213,7 +213,7 @@ def main():
     parser.add_argument('-tqv', '--test_qvecs', default="by1train-context-leadpara-qvecs.npy")
     parser.add_argument('-lr', '--lrate', type=float, default=0.001)
     parser.add_argument('-bt', '--batch', type=int, default=32)
-    parser.add_argument('-ep', '--epochs', type=int, default=10)
+    parser.add_argument('-ep', '--epochs', type=int, default=20)
     parser.add_argument('-ct', '--cats_type', default="scaled")
     parser.add_argument('--cache', action='store_true')
     parser.add_argument('--save', action='store_true')

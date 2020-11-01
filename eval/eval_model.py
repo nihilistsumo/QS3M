46c2689f9d1896e4704b7538ae6f99879c3cf64e
Sumanta  Kashyapi <sk1105@wildcats.unh.edu>
2020-11-01 13:28:53 -0500

    saving sentcats models

--- eval/eval_model.py
+++ eval/eval_model.py
@@ -307,7 +307,7 @@ def main():
 
     parser = argparse.ArgumentParser(description='Run CATS model')
 
-    parser.add_argument('-dd', '--data_dir', default="/home/sk1105/sumanta/CATS_data/")
+    parser.add_argument('-dd', '--data_dir', default="/home/sk1105/sumanta/new_cats_data/")
 
     parser.add_argument('-qt1', '--qry_attn_test1', default="by1train-qry-attn-bal-allpos.tsv")
     parser.add_argument('-aql1', '--art_qrels1', default="/home/sk1105/sumanta/trec_dataset/benchmarkY1/benchmarkY1-train-nodup/train.pages.cbor-article.qrels")
@@ -316,21 +316,21 @@ def main():
     parser.add_argument('-pp1', '--parapairs1', default="/home/sk1105/sumanta/Mule-data/input_data_v2/pairs/train-cleaned-parapairs/by1-train-cleaned.parapairs.json")
     parser.add_argument('-tp1', '--test_pids1', default="by1train-all-pids.npy")
     parser.add_argument('-tv1', '--test_pvecs1', default="by1train-all-paravecs.npy")
-    parser.add_argument('-tq1', '--test_qids1', default="by1train-context-meanall-qids.npy") #change
-    parser.add_argument('-tqv1', '--test_qvecs1', default="by1train-context-meanall-qvecs.npy") #change
+    parser.add_argument('-tq1', '--test_qids1', default="by1train-context-leadpara-qids.npy") #change
+    parser.add_argument('-tqv1', '--test_qvecs1', default="by1train-context-leadpara-qvecs.npy") #change
 
-    parser.add_argument('-qt2', '--qry_attn_test2', default="by1test-qry-attn-bal-allpos-for-eval.tsv")
+    parser.add_argument('-qt2', '--qry_attn_test2', default="by1test-qry-attn-bal-allpos.tsv")
     parser.add_argument('-aql2', '--art_qrels2', default="/home/sk1105/sumanta/trec_dataset/benchmarkY1/benchmarkY1-test-nodup/test.pages.cbor-article.qrels")
     parser.add_argument('-tql2', '--top_qrels2', default="/home/sk1105/sumanta/trec_dataset/benchmarkY1/benchmarkY1-test-nodup/test.pages.cbor-toplevel.qrels")
     parser.add_argument('-hql2', '--hier_qrels2', default="/home/sk1105/sumanta/trec_dataset/benchmarkY1/benchmarkY1-test-nodup/test.pages.cbor-hierarchical.qrels")
     parser.add_argument('-pp2', '--parapairs2', default="/home/sk1105/sumanta/Mule-data/input_data_v2/pairs/test-cleaned-parapairs/by1-test-cleaned.parapairs.json")
     parser.add_argument('-tp2', '--test_pids2', default="by1test-all-pids.npy")
     parser.add_argument('-tv2', '--test_pvecs2', default="by1test-all-paravecs.npy")
-    parser.add_argument('-tq2', '--test_qids2', default="by1test-context-meanall-qids.npy") #change
-    parser.add_argument('-tqv2', '--test_qvecs2', default="by1test-context-meanall-qvecs.npy") #change
+    parser.add_argument('-tq2', '--test_qids2', default="by1test-context-leadpara-qids.npy") #change
+    parser.add_argument('-tqv2', '--test_qvecs2', default="by1test-context-leadpara-qvecs.npy") #change
 
     parser.add_argument('-mt', '--model_type', default="cats") #cats, scaled, abl
-    parser.add_argument('-mp', '--model_path', default="/home/sk1105/sumanta/CATS/saved_models/cats_meanall_b32_l0.00001_i3.model") #change
+    parser.add_argument('-mp', '--model_path', default="/home/sk1105/sumanta/cats_deploy/model/saved_models/cats_leadpara_b32_l0.00001_i4.model") #change
 
     '''
     parser.add_argument('-dd', '--data_dir', default="/home/sk1105/sumanta/CATS_data/")

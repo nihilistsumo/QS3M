d34734ed7ee79f4affe1c2254dd6595759546a6c
Sumanta  Kashyapi <sk1105@wildcats.unh.edu>
2020-11-28 11:49:31 -0500

    validation code update

--- eval/eval_model.py
+++ eval/eval_model.py
@@ -329,8 +329,8 @@ def main():
     parser.add_argument('-tq2', '--test_qids2', default="by1test-context-leadpara-qids.npy") #change
     parser.add_argument('-tqv2', '--test_qvecs2', default="by1test-context-leadpara-qvecs.npy") #change
 
-    parser.add_argument('-mt', '--model_type', default="cats") #cats, scaled, abl
-    parser.add_argument('-mp', '--model_path', default="/home/sk1105/sumanta/cats_deploy/model/saved_models/cats_leadpara_b32_l0.00001_i3.model") #change
+    parser.add_argument('-mt', '--model_type', default="abl") #cats, scaled, abl
+    parser.add_argument('-mp', '--model_path', default="/home/sk1105/sumanta/cats_deploy/model/saved_models/abl_cats_p1p2pd_leadpara_b32_l0.00001_i3.model") #change
 
     '''
     parser.add_argument('-dd', '--data_dir', default="/home/sk1105/sumanta/CATS_data/")
